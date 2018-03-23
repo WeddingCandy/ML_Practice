9e02c375c691f4f73e3abfbf68e734e59f60b39a
thinkpad <chenyouwenxyz@hotmail.com>
2018-03-23 16:46:21 +0800

    23rd March in Thinkpad

--- Crawler/crawler_label.py
+++ Crawler/crawler_label.py
@@ -12,23 +12,26 @@ headers = {
 excel_sheet = pd.read_excel('C:/Users/thinkpad/Desktop/labels.xlsx',sheet_name = 'url',header= 0)
 # excel_sheet = pd.read_excel(r'/Users/Apple/Desktop/working/0 华院资料/HIVE库表/9 用户浏览标签 -20180319.xlsx',sheet_name = 'url',header= 0)
 excel_sheet_extract = excel_sheet.ix[excel_sheet['数据源类别\n1为网站\n2为APP']<2,['行业一级大类','行业二级大类', 'webname']].reset_index()
+excel_sheet_extract['label_contents'] = 'NULL'
 colums_aim = excel_sheet.ix[excel_sheet['数据源类别\n1为网站\n2为APP']<2 ,3:4].reset_index()
-labels_ori = "http://" + colums_aim
+labels_ori = "http://" + colums_aim.iloc[:,1:]
 labels = labels_ori.values.tolist()
-for id in range(len(labels)):
+count_failure = 0 ;count_success = 0
+for id in range(0,len(labels)):
     try:
-        req = requests.get(url=labels[4][0], headers=headers ,timeout=2)
-        print('have read'+str(id))
+        req = requests.get(url=labels[id][0], headers=headers ,timeout=2)
+        html = req.text.encode(req.encoding).decode(req.encoding)
+        print('have read'+str(id+1))
     except Exception  as e:
         print("Exception: {}".format(e))
-        print('第%d个label无法爬取' %(str(id)))
+        print('第%d个label无法爬取' %(id+1))
+        count_failure +=1
         continue
     else:
         data =req.json
 
 
     # html0 =req.content.decode('gbk', 'ignore')
-    html = req.text.encode(req.encoding).decode(req.encoding)
     bf = BeautifulSoup(html, "html.parser")
     meta_all = bf.find_all('head')
     meta_tostring = str(meta_all)
@@ -41,5 +44,9 @@ for id in range(len(labels)):
         print(i.group() + '\n')
         keep.append(i.group())
     keep = list(set(keep))
-    print('第%s个label已完成' %(str(id)))
-    excel_sheet_extract['label_contents'] =
+    print('第%s个label已完成' %(id+1))
+    count_success +=1
+    excel_sheet_extract.ix[id:id,'label_contents'] = str(keep)
+print('成功数为：%d,成功率为：%d\%' %(count_success,count_success/(count_failure+count_success)*100))
+path = 'C:\\Users\\thinkpad\\Desktop\\result.csv'
+excel_sheet_extract.to_csv(path )
